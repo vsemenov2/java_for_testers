a38a7be22e821b8eab9dbeda8b5ee36bd0567ca2
vsemenov1 <vladyxxxa@gmail.com>
2023-09-21 12:35:14 +0300

    Задание 2, v2

--- sanbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sanbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,11 +2,27 @@ package ru.stqa.geometry.figures;
 
 public record Triangle() {
      public static void SquareTriangle(double sideA, double sideB, double sideC) {
-        var perimeterTriangle = (sideA + sideB + sideC) / 2;
-        var areaTriangle = Math.sqrt(perimeterTriangle * (perimeterTriangle - sideA) * (perimeterTriangle - sideB) * (perimeterTriangle - sideC));
+         var perimeterTriangle = getPerimeterTriangle(sideA, sideB, sideC);
+         var areaTriangle = getTriangle(sideA, sideB, sideC, perimeterTriangle);
         System.out.println(String.format("Периметр треугольника со сторонами %f  %f  %f  = %f ", sideA, sideB, sideC, perimeterTriangle));
         System.out.println(String.format("Площадь треугольника по формуле Герона = %f ", areaTriangle));
 
+
+         /*var perimeterTriangle = (sideA + sideB + sideC) / 2;
+         System.out.println("Периметр треугольника = " + perimeterTriangle);
+         System.out.println("Площадь треугольника по формуле Герона =  " + Math.sqrt(perimeterTriangle * (perimeterTriangle - sideA) * (perimeterTriangle - sideB) * (perimeterTriangle - sideC)));*/
+
+
+
+
             }
+
+    public static double getPerimeterTriangle(double sideA, double sideB, double sideC) {
+        return (sideA + sideB + sideC) / 2;
+    }
+
+    public static double getTriangle(double sideA, double sideB, double sideC, double perimeterTriangle) {
+        return Math.sqrt(perimeterTriangle * (perimeterTriangle - sideA) * (perimeterTriangle - sideB) * (perimeterTriangle - sideC));
+    }
 
 }
--- sanbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sanbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -17,7 +17,12 @@ public class SquareTests {
         Assertions.assertEquals(20.0, new Square(5.0).perimeter());
     }
     @Test
-    void canCalculateTriangle(){
-        Assertions.assertEquals(20.0, Triangle.SquareTriangle(5.0, 6.0, 6,0));
+    void canCalculatePerimeterTriangle(){
+        Assertions.assertEquals(8.0, Triangle.getPerimeterTriangle(5.,5.,6.));
+
+    }
+    @Test
+    void canCalculateTriangle() {
+        Assertions.assertEquals(24.49489742783178, Triangle.getTriangle(5.0, 6.0, 7.0, 10));
     }
 }
