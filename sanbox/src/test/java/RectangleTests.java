3edd0631186cf72517a6a907beb83bd2048e6dfc
vsemenov1 <vladyxxxa@gmail.com>
2023-10-04 20:21:45 +0300

    Лекция 2.4. Сравнение объектов (метод equals)

--- sanbox/src/test/java/RectangleTests.java
+++ sanbox/src/test/java/RectangleTests.java
@@ -5,11 +5,25 @@ import ru.stqa.geometry.figures.Square;
 
 public class RectangleTests {
     @Test
-    void cannotCreateRectangleWithNegativeSide(){
+    void cannotCreateRectangleWithNegativeSide() {
         try {
             new Rectangle(-5.0, 3.0);
             Assertions.fail();
         } catch (IllegalArgumentException exception) {
         }
+    }
+
+    @Test
+    void testEquality()    {
+        var r1 = new Rectangle(5.0, 4.0);
+        var r2 = new Rectangle(5.0, 4.0);
+        Assertions.assertEquals(r1, r2);
+    }
+
+    @Test
+    void testEqualit2()    {
+        var r1 = new Rectangle(5.0, 4.0);
+        var r2 = new Rectangle(4.0, 5.0);
+        Assertions.assertEquals(r1, r2);
     }
 }
--- sanbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sanbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -36,5 +36,28 @@ public class SquareTests {
     @Test
     void canCalculateTriangle() {
         Assertions.assertEquals(14.696938456699069, Triangle.getTriangle(5.0, 6.0, 7.0));
+
+    }
+
+    @Test
+    void testEquality()    {
+        var s1 = new Square(5.0);
+        var s2 = new Square(5.0);
+        Assertions.assertEquals(s1, s2);
+    }
+
+    @Test
+    void testNonEquality()    {
+        var s1 = new Square(5.0);
+        var s2 = new Square(4.0);
+        Assertions.assertNotEquals(s1, s2);
+    }
+
+    @Test
+    void testFail()    {
+        var s1 = new Square(5.0);
+        var s2 = new Square(5.0);
+        Assertions.assertTrue(s1.equals(s2));
     }
 }
+
