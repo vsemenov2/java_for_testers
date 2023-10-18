5c6d5f80fc268947868a75f720110b01027d33e6
vsemenov1 <vladyxxxa@gmail.com>
2023-10-18 16:38:47 +0300

    Лекция 4.5. Параметризованные тесты

--- addressbook_web_tests/src/test/java/test/TestBase.java
+++ addressbook_web_tests/src/test/java/test/TestBase.java
@@ -16,12 +16,13 @@ public class TestBase {
             app.init(System.getProperty("browser", "chrome"));
         }
     }
-    public String randomString(int n) {
+    public static String randomString(int n) {
         var rnd = new Random();
         var result = "";
         for (int i = 0; i < n; i++){
             result = result + (char)('a' + rnd.nextInt(26));
         }
+
         return result;
     }
 
