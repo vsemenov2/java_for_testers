e6c183693ebda4e94c0fc26de999c178b80ac62a
vsemenov1 <vladyxxxa@gmail.com>
2023-10-26 19:00:52 +0300

    Лекция 5.4 - 5.7

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -2,9 +2,16 @@ package generator;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import common.CommonFunctions;
 import model.GroupData;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Generator {
@@ -22,7 +29,7 @@ public class Generator {
     int count;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        var generator = new Generator();
         JCommander.newBuilder()
                 .addObject(generator)
@@ -31,7 +38,7 @@ public class Generator {
        generator.run();
     }
 
-    private void run() {
+    private void run() throws IOException {
         var data = generate();
         save(data);
     }
@@ -63,7 +70,26 @@ public class Generator {
         return null;
     }
 
-    private void save(Object data) {
+    private void save(Object data) throws IOException {
+        if ("json".equals(format)) {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            var json = mapper.writeValueAsString(data);
+
+            try (var write = new FileWriter(output)) {
+                write.write(json);
+            }
+
+
+        } if ("yaml".equals(format))  {
+            var mapper = new YAMLMapper();
+            mapper.writeValue(new File(output), data);
+        } if ("xml".equals(format))  {
+            var mapper = new XmlMapper();
+            mapper.writeValue(new File(output), data);
+        } else {
+            throw new IllegalArgumentException("Неизвестный формат данных" + format);
+        }
 
     }
 }
--- addressbook_web_tests/src/test/java/test/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/test/GroupCreationTests.java
@@ -1,5 +1,8 @@
 package test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import common.CommonFunctions;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
@@ -7,6 +10,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -14,21 +24,33 @@ import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-    public static List<GroupData> groupProvider() {
+    public static List<GroupData> groupProvider() throws IOException {
         var result = new ArrayList<GroupData>();
-        for (var name : List.of("", "group name")){
-                for (var header : List.of("", "group header")) {
-                    for (var footer : List.of("", "group footer")){
-                        result.add(new GroupData().withName(name).withHeader(header).withFooter(footer));
-                    }
-                }
-            }
-            for (int i = 0; i < 5; i++) {
-                result.add(new GroupData()
-                        .withName(CommonFunctions.randomString(i * 10))
-                        .withHeader(CommonFunctions.randomString(i * 10))
-                        .withFooter(CommonFunctions.randomString(i * 10)));
-            }
+//        for (var name : List.of("", "group name")){
+//                for (var header : List.of("", "group header")) {
+//                    for (var footer : List.of("", "group footer")){
+//                        result.add(new GroupData().withName(name).withHeader(header).withFooter(footer));
+//                    }
+//                }
+//            }
+
+//        var json = "";
+//        try (var reader = new FileReader("groups.json");
+//             var breader = new BufferedReader(reader)
+//        )
+//        {
+//            var line = breader.readLine();
+//            while (line != null) {
+//                json = json + line;
+//                line = breader.readLine();
+//
+//            }
+//
+//        }
+
+        var mapper = new XmlMapper();
+        var value = mapper.readValue(new File("groups.xml"), new TypeReference<List<GroupData>>() {});
+        result.addAll(value);
 
         return result;
     }
