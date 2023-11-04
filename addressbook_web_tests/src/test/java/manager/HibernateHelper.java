15dcfdf678303cb1ce414306f1409c7bc5950084
vsemenov1 <vladyxxxa@gmail.com>
2023-11-04 15:08:39 +0300

    Лекция 6.3. ORM: создание и изменение записей в базе данных

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -33,14 +33,35 @@ public class HibernateHelper extends HelperBase {
         return result;
     }
 
-    @NotNull
     private static GroupData convert(GroupRecord record) {
         return new GroupData("" + record.id, record.name, record.header, record.footer);
+    }
+
+    private static GroupRecord convert(GroupData data) {
+        var id = data.id();
+        if ("".equals(id)){
+            id = "0";
+        }
+        return new GroupRecord(Integer.parseInt(id), data.name(), data.header(), data.footer());
     }
 
     public List<GroupData> getGroupList() {
         return convertList(sessionFactory.fromSession(session -> {
             return  session.createQuery("from GroupRecord", GroupRecord.class).list();
         }));
+    }
+
+    public long getGroupCount() {
+        return sessionFactory.fromSession(session -> {
+            return  session.createQuery("select count (*) from GroupRecord", Long.class).getSingleResult();
+        });
+    }
+
+    public void createGroup(GroupData groupData) {
+        sessionFactory.inSession(session -> {
+            session.getTransaction().begin();
+            session.persist(convert(groupData));
+            session.getTransaction().commit();
+        });
     }
 }
