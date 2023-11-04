053a643332c907884028ea85911e0880ad88e1a7
vsemenov1 <vladyxxxa@gmail.com>
2023-11-04 13:54:09 +0300

    Лекция 6.1. Драйвер базы данных. Получение данных из БД

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -15,6 +15,8 @@ public class ApplicationManager  {
     private GroupHelper groups;
     private ContactHelper contacts;
 
+    private JdbcHelper jdbc;
+
     private  Properties properties;
 
     public void init(String browser, Properties properties) {
@@ -54,6 +56,13 @@ public class ApplicationManager  {
             contacts = new ContactHelper(this);
         }
         return contacts;
+    }
+
+    public JdbcHelper jdbc() {
+        if (jdbc == null){
+            jdbc = new JdbcHelper(this);
+        }
+        return jdbc;
     }
 
     protected boolean isElementPresent(By locator) {
--- addressbook_web_tests/src/test/java/test/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/test/GroupCreationTests.java
@@ -55,21 +55,31 @@ public class GroupCreationTests extends TestBase {
         return result;
     }
 
+    public static List<GroupData> singRandomGroup() {
+       return List.of(new GroupData()
+                .withName(CommonFunctions.randomString(10))
+                .withHeader(CommonFunctions.randomString(20))
+                .withFooter(CommonFunctions.randomString(30)));
+    }
+
     @ParameterizedTest
-    @MethodSource("groupProvider")
-    public void canCreateMultipleGroups(GroupData group) {
-        var oldGroups = app.groups().getList();
+    @MethodSource("singRandomGroup")
+    public void canCreateGroup(GroupData group) {
+        var oldGroups = app.jdbc().getGroupList();
         app.groups().createGroup(group);
-        var newGroups = app.groups().getList();
+        var newGroups = app.jdbc().getGroupList();
         Comparator<GroupData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
         newGroups.sort(compareById);
+        var maxId = newGroups.get(newGroups.size() - 1).id();
 
         var expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(newGroups.get(newGroups.size() -1).id()).withHeader("").withFooter(""));
+        expectedList.add(group.withId(maxId));
         expectedList.sort(compareById);
-        Assertions.assertEquals(newGroups,expectedList);
+        Assertions.assertEquals(newGroups, expectedList);
+        
+
     }
 
     public static List<GroupData> negativeGroupProvider() {
