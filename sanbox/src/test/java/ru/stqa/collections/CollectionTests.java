9524d84558a72c48bbff2f688988042f03aa080b
vsemenov1 <vladyxxxa@gmail.com>
2023-11-14 20:14:39 +0300

    Лекция 7.4

--- sanbox/src/test/java/ru/stqa/collections/CollectionTests.java
+++ sanbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionTests {
 
     @Test
     void arrayTests() {
-        var array = new String[]{"a", "b", "c"};
+        var array = new String[]{"a", "b", "c", "a"};
         //var array = new String[3];
-        Assertions.assertEquals(3, array.length);
+        Assertions.assertEquals(4, array.length);
         Assertions.assertEquals("a", array[0]);
 
 
@@ -30,5 +32,15 @@ public class CollectionTests {
         list.set(0, "d");
         Assertions.assertEquals("d", list.get(0));
 
+    }
+
+    @Test
+    void setTests() {
+        var set = new HashSet<>(List.of("a", "b", "c", "a"));
+        Assertions.assertEquals(3, set.size());
+
+
+        set.add("d");
+        Assertions.assertEquals(4, set.size());
     }
 }
