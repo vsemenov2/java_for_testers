0927850138aa4b5f6ea7bb9b1cf9188df21fe9b0
vsemenov1 <vladyxxxa@gmail.com>
2023-11-14 21:35:43 +0300

    Лекция 7.6. Словари, или ассоциативные массивы (Map)

--- addressbook_web_tests/src/test/java/test/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/test/ContactInfoTests.java
@@ -1,5 +1,6 @@
 package test;
 
+import model.ContactDate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,15 +13,13 @@ public class ContactInfoTests extends TestBase {
     @Test
     void testPhones() {
         var contacts = app.hbm().getContactList();
-        var contact = contacts.get(0);
-        var phones = app.contacts().getPhones(contact);
-        var expected = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
-                .filter(s -> s != null && ! "".equals(s))
-                .collect(Collectors.joining("\n"));
-        Assertions.assertEquals(expected, phones);
+        var expected = contacts.stream().collect(Collectors.toMap(ContactDate::id, contact ->
+                    Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                            .filter(s -> s != null && !"".equals(s))
+                            .collect(Collectors.joining("\n"))
+                ));
+        var phones = app.contacts().getPhones();
+        Assertions.assertEquals(expected, phones );
 
     }
-
-
-
 }
--- sanbox/src/test/java/ru/stqa/collections/CollectionTests.java
+++ sanbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -3,10 +3,7 @@ package ru.stqa.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class CollectionTests {
 
@@ -42,5 +39,17 @@ public class CollectionTests {
 
         set.add("d");
         Assertions.assertEquals(4, set.size());
+    }
+
+    @Test
+    void testMap(){
+        var digits = new HashMap<Character, String>();
+        digits.put('1', "one");
+        digits.put('2', "two");
+        digits.put('3', "three");
+
+        Assertions.assertEquals("one", digits.get('1'));
+        digits.put('1', "один");
+        Assertions.assertEquals("один", digits.get('1'));
     }
 }
