09f8e80a95d2cb1b20c91c1f2b4a2c996adf900e
vsemenov1 <vladyxxxa@gmail.com>
2023-11-21 23:41:32 +0300

    Лекция 9.5. REST API для управления баг-трекером Mantis

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
     private RegistrationHelper registrationHelper;
     private JamesApiHelper jamesApiHelper;
     private DeveloperMailHelper developerMailHelper;
+    private RestApiHelper restApiHelper;
 
 
     public void init(String browser, Properties properties) {
@@ -88,9 +89,7 @@ public class ApplicationManager {
         }
         return developerMailHelper;
     }
-    public String property(String name){
-        return properties.getProperty(name);
-    }
+
 
     public RegistrationHelper registration() {
         if (registrationHelper == null) {
@@ -99,5 +98,16 @@ public class ApplicationManager {
         return registrationHelper;
     }
 
+
+    public RestApiHelper rest() {
+        if (restApiHelper == null) {
+            restApiHelper = new RestApiHelper(this);
+        }
+        return restApiHelper;
+    }
+
+    public String property(String name){
+        return properties.getProperty(name);
+    }
 
 }
