dcfa25001c3897b7897cb9786c2c523d648f9b14
vsemenov1 <vladyxxxa@gmail.com>
2023-11-22 00:12:35 +0300

    Лекция 9.6. SOAP API для управления баг-трекером Mantis

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
     private JamesApiHelper jamesApiHelper;
     private DeveloperMailHelper developerMailHelper;
     private RestApiHelper restApiHelper;
+    private SoapApiHelper soapApiHelper;
 
 
     public void init(String browser, Properties properties) {
@@ -104,6 +105,13 @@ public class ApplicationManager {
             restApiHelper = new RestApiHelper(this);
         }
         return restApiHelper;
+    }
+
+    public SoapApiHelper soap() {
+        if (soapApiHelper == null) {
+            soapApiHelper = new SoapApiHelper(this);
+        }
+        return soapApiHelper;
     }
 
     public String property(String name){
--- mantis_tests/src/test/java/ru/stqa/mantis/test/IssueCreationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/test/IssueCreationTests.java
@@ -7,7 +7,7 @@ import ru.stqa.mantis.model.IssueData;
 public class IssueCreationTests extends TestBase{
     @Test
     void canCreateIssue(){
-        app.rest().createIssue(new IssueData()
+        app.soap().createIssue(new IssueData()
                 .withSummary(CommonFunctions.randomString(10))
                 .withDescription(CommonFunctions.randomString(50))
                 .withProject(1L));
