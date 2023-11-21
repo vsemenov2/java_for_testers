1a45e08bd296c4e0f5ec9d15be8f6890a80ff55f
vsemenov1 <vladyxxxa@gmail.com>
2023-11-22 00:25:43 +0300

    Задание №19

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/RestApiHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/RestApiHelper.java
@@ -4,10 +4,14 @@ import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
 import io.swagger.client.Configuration;
 import io.swagger.client.api.IssuesApi;
+import io.swagger.client.api.UserApi;
 import io.swagger.client.auth.ApiKeyAuth;
 import io.swagger.client.model.Identifier;
 import io.swagger.client.model.Issue;
+import io.swagger.client.model.User;
+import io.swagger.client.model.UserAddResponse;
 import ru.stqa.mantis.model.IssueData;
+import ru.stqa.mantis.model.UserRegistration;
 
 public class RestApiHelper extends HelperBase {
     public RestApiHelper(ApplicationManager manager) {
@@ -34,6 +38,20 @@ public class RestApiHelper extends HelperBase {
             apiInstance.issueAdd(issue);
         } catch (ApiException e) {
            new RuntimeException(e);
+        }
+    }
+
+    public void addUser(UserRegistration registration) {
+        UserApi apiInstance = new UserApi();
+        User user = new User();
+        user.setUsername(registration.username());
+        user.setRealName(registration.username());
+        user.setEmail(registration.email());
+        try {
+            UserAddResponse result = apiInstance.userAdd(user);
+            System.out.println(result);
+        } catch (ApiException e) {
+            new RuntimeException(e);
         }
     }
 }
--- mantis_tests/src/test/java/ru/stqa/mantis/test/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/test/UserRegistrationTests.java
@@ -1,18 +1,13 @@
 package ru.stqa.mantis.test;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.stqa.mantis.common.CommonFunctions;
-import ru.stqa.mantis.model.DeveloperMailUser;
 import ru.stqa.mantis.model.UserRegistration;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class UserRegistrationTests extends TestBase {
 
@@ -37,6 +32,22 @@ public class UserRegistrationTests extends TestBase {
         app.driver().get(url);
 
         app.registration().canConfirmUser(CommonFunctions.randomString(10), "password");
+        app.http().login(registration.username(), "password");
+        Assertions.assertTrue(app.http().isLoggedIn());
+    }
+
+    @ParameterizedTest
+    @MethodSource("singleUser")
+    void canCreateUserRestApi(UserRegistration registration) {
+        app.jamesApi().addUser(registration.email(), "password");
+        app.rest().addUser(registration);
+        var messages = app.mail().receive(registration.email(), "password", Duration.ofSeconds(60));
+        var text = messages.get(0).content();
+        var url = CommonFunctions.extract(text);
+
+        app.driver().get(url);
+
+        app.registration().canConfirmUser(registration.username(), "password");
         app.http().login(registration.username(), "password");
         Assertions.assertTrue(app.http().isLoggedIn());
     }
